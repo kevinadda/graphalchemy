c3fec3eced205a8b6e6b316377cc3f39c09744c9
chefkevin <kadda@chefjerome.com>
2013-10-18 15:54:02 +0200

    feat(types): Integer 'interface' distributing to Integer, BigInteger and Long.

--- graphalchemy/blueprints/types.py
+++ graphalchemy/blueprints/types.py
@@ -33,7 +33,6 @@ class Type(object):
         """
         return value
 
-
     def to_db(self, value):
         """ Coerces the value to the appropriate database type.
 
@@ -86,9 +85,27 @@ class Numeric(Type):
         return super(Numeric, self).validate(value)
 
 
-class Integer(Numeric):
+class Integer(Type):
 
     name_db = "Integer.class"
+
+    def __init__(self, n_digits=10):
+        """ Defines the specifications of the Type.
+
+        This class interfaces python conception of Integer with java
+        corresponding concepts: Integer, BigInteger and Long.
+        Class casting allows to keep the python conception of Integer,
+        and to distribute it to java classes.
+
+        :param n_digits: The maximal number of digits composing the value.
+        :type n_digits: int
+        """
+        if 10 < n_digits < 20:
+            self.__class__ = Long
+        elif n_digits >= 20:
+            self.__class__ = BigInteger
+
+        self.n_digits = n_digits
 
     def to_py(self, value):
         return int(value)
@@ -96,7 +113,17 @@ class Integer(Numeric):
     def validate(self, value):
         if not isinstance(value, int):
             return False, [u'Wrong type : expected int, got '+str(type(value))]
-        return super(Integer, self).validate(value)
+        return True
+
+
+class Long(Integer):
+
+    name_db = "Long.class"
+
+
+class BigInteger(Integer):
+
+    name_db = "BigInteger.class"
 
 
 class Float(Numeric):
